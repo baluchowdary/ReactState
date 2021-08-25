221c1d10bb978ebe6268ff3d3cc96a82b1d622ae
baluchowdary <bhardwaja.kollu@gmail.com>
2021-08-25 15:10:42 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+class App extends Component {
+    constructor(){
+        super();
+        this.state={ displayBio: false };
+
+        console.log('readMore this', this);
+
+        this.readMore = this.readMore.bind(this)
+    }
+    readMore(){
+        console.log('readMore this', this);
+
+        //this.setState({displayBio : true});
+    }
+    render(){
+        return (
+            <div>
+                <h1>Iam kollu</h1>
+                <p>Hello kollu1</p>
+                <p>bhardwaja</p>
+                {
+                this.state.displayBio ? (
+                    <div>
+                        <p>I am from block1</p>
+                        <p>i am kollu from block1</p>
+                        </div>
+                    ) : (
+                        <div>
+                            <button onClick={this.readMore}>Read More</button>
+                            </div>
+                    )
+                }
+            </div>
+        ) //return
+    } //render end
+
+} //class end
 
 export default App;
